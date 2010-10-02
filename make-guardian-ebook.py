689a43baf470afd6277ad03113419bf180698806
Mark Longair <mhl@pobox.com>
2010-10-02 10:09:52 +0200

    The byline may also contain Unicode characters

--- make-guardian-ebook.py
+++ make-guardian-ebook.py
@@ -140,7 +140,7 @@ with open(today_filename) as fp:
 
                 page_fp.write('<h1>{h}</h1>\n'.format(h=headline.encode('UTF-8')))
                 if byline:
-                    page_fp.write('<h4>By {b}</h4>'.format(b=byline))
+                    page_fp.write('<h4>By {b}</h4>'.format(b=byline.encode('UTF-8')))
                 if standfirst:
                     page_fp.write('<p><em>{sf}</em></p>'.format(sf=standfirst.encode('UTF-8')))
 
